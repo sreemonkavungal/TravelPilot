6bc83476dea70e672d41bb6bf93bc6e9e1c6272c
SREEMON KS <sreemonkavungal@gmail.com>
2025-06-18 15:51:48 +0530

    Home - Content updated

--- src/components/ContentSection.jsx
+++ src/components/ContentSection.jsx
@@ -1,90 +1,4 @@
-// import { CloudArrowUpIcon, LockClosedIcon, ServerIcon } from '@heroicons/react/20/solid'
 import HomeIMG from '../assets/images/HomeImage.png';
-
-
-//     const features = [
-//       {
-//         name: 'Push to deploy.',
-//         description:
-//           'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis suscipit eaque, iste dolor cupiditate blanditiis ratione.',
-//         icon: CloudArrowUpIcon,
-//       },
-//       {
-//         name: 'SSL certificates.',
-//         description: 'Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem cupidatat commodo.',
-//         icon: LockClosedIcon,
-//       },
-//       {
-//         name: 'Database backups.',
-//         description: 'Ac tincidunt sapien vehicula erat auctor pellentesque rhoncus. Et magna sit morbi lobortis.',
-//         icon: ServerIcon,
-//       },
-//     ]
-    
-//     export default function ContentSection() {
-//       return (
-//         <div className="overflow-hidden bg-white py-24 sm:py-32">
-//           <div className="mx-auto max-w-7xl px-6 lg:px-8">
-//             <div className="mx-auto grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 sm:gap-y-20 lg:mx-0 lg:max-w-none lg:grid-cols-2 lg:items-center">
-//               <div className="lg:pt-4 lg:pr-8">
-//                 <div className="lg:max-w-lg">
-//                   <h2 className="text-base/7 font-semibold text-indigo-600">Deploy faster</h2>
-//                   <p className="mt-2 text-4xl font-semibold tracking-tight text-pretty text-gray-900 sm:text-5xl">
-//                     A better workflow
-//                   </p>
-//                   <p className="mt-6 text-lg/8 text-gray-600">
-//                     Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis suscipit eaque,
-//                     iste dolor cupiditate blanditiis ratione.
-//                   </p>
-//                   <dl className="mt-10 max-w-xl space-y-8 text-base/7 text-gray-600 lg:max-w-none">
-//                     {features.map((feature) => (
-//                       <div key={feature.name} className="relative pl-9">
-//                         <dt className="inline font-semibold text-gray-900">
-//                           <feature.icon aria-hidden="true" className="absolute top-1 left-1 size-5 text-indigo-600" />
-//                           {feature.name}
-//                         </dt>{' '}
-//                         <dd className="inline">{feature.description}</dd>
-//                       </div>
-//                     ))}
-//                   </dl>
-//                 </div>
-//               </div>
-//               {/* <img
-//                 alt="Product screenshot"
-//                 src={HomeIMG}
-//                 width={2000} 
-//                 height={1400}
-//                 className="w-[48rem] max-w-none rounded-xl shadow-xl ring-1 ring-white sm:w-[57rem] md:-ml-4 lg:-ml-0"
-//               /> */}
-
-//           {/* Right: Image */}
-//           <div className="flex justify-end">
-//             <img
-//               src={HomeIMG}
-//               alt="Product screenshot"
-//               className="w-full max-w-10xl"
-//             />
-//           </div>
-
-
-//             </div>
-//           </div>
-//         </div>
-//       )
-//     }
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 export default function Example() {
@@ -107,10 +21,10 @@ export default function Example() {
             </svg>
             <div className="mx-auto max-w-md text-center lg:mx-0 lg:flex-auto lg:py-32 lg:text-left">
               <h2 className="text-3xl font-semibold tracking-tight text-balance text-black sm:text-4xl">
-                Boost your productivity. Start using our app today.
+                Explore the world with confidence.
               </h2>
               <p className="mt-6 text-lg/8 text-pretty text-gray-600">
-                Ac euismod vel sit maecenas id pellentesque eu sed consectetur. Malesuada adipiscing sagittis vel nulla.
+                Plan every journey with ease, safety, and unforgettable moments. TravelPilot is your trusted guide wherever you go.
               </p>
               <div className="mt-10 flex items-center justify-center gap-x-6 lg:justify-start">
                 <a
