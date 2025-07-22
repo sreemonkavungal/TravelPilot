0a7d8509cf22fa083015cd207e2a19a4c31db8c8
SREEMON KS <sreemonkavungal@gmail.com>
2025-07-22 15:52:31 +0530

    hover effect - NotFound Page

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -12,7 +12,7 @@
                 </p>
             <div className="mt-10 flex items-center justify-center gap-x-6">
                 <a href="/"
-                className="rounded-md bg-green-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-xs">
+                className="rounded-md bg-green-600 px-3.5 py-2.5 text-sm font-semibold text-white hover:bg-green-800 shadow-xs">
                     Go back home
                 </a>
             </div>
